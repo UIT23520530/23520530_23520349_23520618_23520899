7c854f0d10994780a70d78e4c6491b99987f71b3
UIT23520530 <23520530@gm.uit.edu.vn>
2024-05-14 23:24:25 +0700

    Update snake_score.cpp

--- snake_score.cpp
+++ snake_score.cpp
@@ -1,214 +1,216 @@
-#include<iostream>
-#include<vector>
-#include<windows.h>
-#include<conio.h>
-#include<cstdlib>
-#include<cmath>
+#include <iostream>
+#include <vector>
+#include <windows.h>
+#include <conio.h>
+#include <cstdlib>
+#include <cmath>
 
 using namespace std;
-void gotoxy( int column, int line );
-class  Snake{
-     int length;
-     vector<pair<int,int>>a;
-     public:  
-     static int h;  
-     Snake()
-     {
-        length=3;
-        a.resize(length);
-        a[0].first=10;a[0].second=10;
-        a[1].first=11;a[1].second=10;
-        a[2].first=12;a[2].second=10;
-        
-     }
-     
-     void Draw(pair <int,int> x)
-     {  
-            gotoxy(x.first,x.second);
-            cout<<" ";
-            gotoxy(a[0].first,a[0].second);
-            printf("x");
-    
 
-     }
-     void Move(int direction,pair<int,int> & x)
-     {   if(abs(h-direction)==2) direction=h;
-         h=direction;
-         x.first=a[length-1].first;
-         x.second=a[length-1].second;
-         for(int i=length-1;i>0;i--)
-         {
-            a[i]=a[i-1];
-         }
-         if(direction==0)  a[0].first=a[0].first+1;
-         if(direction==1)  a[0].second=a[0].second+1;
-         if(direction==2)  a[0].first=a[0].first-1;
-         if(direction==3)  a[0].second=a[0].second-1;
-     }
-     void up_length()
-     {
-        if(a[length-1].first==a[length-2].first)
-        {
-            if(a[length-1].second<a[length-2].second)
-            {
-                length++;
-                a[length-1].first=a[length-2].first;
-                a[length-1].second=a[length-2].second-1;
+void gotoxy(int column, int line);
+
+class Snake {
+    int length;
+    vector<pair<int, int>> snake;
+
+public:
+    int score = 0;
+    static int h;
+
+    Snake() {
+        length = 3;
+        snake.resize(length);
+        snake[0].first = 10; snake[0].second = 10;
+        snake[1].first = 9; snake[1].second = 10;
+        snake[2].first = 8; snake[2].second = 10;
+    }
+
+    void Draw(pair<int, int> x) {
+        gotoxy(x.first, x.second); // vi tri cua phan tu cuoi cung trong con ran
+        cout << " ";
+        for (const auto& body : snake) {
+            gotoxy(body.first, body.second);
+            cout << "x";
+        }
+    }
+
+    void Move(int direction, pair<int, int>& x) {
+        if (abs(h - direction) == 2) direction = h;
+        h = direction;
+        x.first = snake[length - 1].first;
+        x.second = snake[length - 1].second;
+        for (int i = length - 1; i > 0; i--) {
+            snake[i] = snake[i - 1];
+        }
+        if (direction == 0)  snake[0].first = snake[0].first + 1;
+        if (direction == 1)  snake[0].second = snake[0].second + 1;
+        if (direction == 2)  snake[0].first = snake[0].first - 1;
+        if (direction == 3)  snake[0].second = snake[0].second - 1;
+    }
+
+    void IncreasingLength() {
+        pair<int, int> new_body;
+        if (snake[length - 1].first == snake[length - 2].first) {
+            if (snake[length - 1].second < snake[length - 2].second) {
+                new_body = { snake[length - 1].first, snake[length - 1].second - 1 };
             }
             else {
-                 length++;
-                a[length-1].first=a[length-2].first;
-                a[length-1].second=a[length-2].second+1;
+                new_body = { snake[length - 1].first, snake[length - 1].second + 1 };
             }
-
         }
         else {
-             if(a[length-1].first<a[length-2].first)
-            {
-                length++;
-                a[length-1].first=a[length-2].first-1;
-                a[length-1].second=a[length-2].second;
+            if (snake[length - 1].first < snake[length - 2].first) {
+                new_body = { snake[length - 1].first - 1, snake[length - 1].second };
             }
-            else{
-                length++;
-                a[length-1].first=a[length-2].first+1;
-                a[length-1].second=a[length-2].second;
+            else {
+                new_body = { snake[length - 1].first + 1, snake[length - 1].second };
             }
         }
-     }
+        snake.push_back(new_body);
+        length++;
+    }
 
-     friend class apple;
-     friend class border;
-};
-class apple
-{  
-   int x,y;
-   public:
-   apple(Snake b)
-   {
-   an:x=rand()%72+1;
-      y=rand()%17+1;
-      for(int i=0;i<b.length;i++)
-      {
-        if(x==b.a[i].first && y==b.a[i].second)
+    bool IsOnBody()
+    {
+        for (int i = 1; i < length; i++)
         {
-            goto an;
+            if (snake[0] == snake[i])
+                return true;
         }
-      }
-       
-
-   }
-   void Draw()
-   {
-       gotoxy(x,y);
-       cout<<"0";
-   }
-   int eat_apple(Snake b)
-   {
-       if(b.a[0].first==x && b.a[0].second==y)
-       {
-         return 1;
-       }
-       return 0;
-   }
-   void delete_apple(Snake b)
-   {  gotoxy(x,y);
-      cout<<" ";
-      an: x=rand()%72+1;
-      y=rand()%17+1;
-      for(int i=0;i<b.length;i++)
-      {
-        if(x==b.a[i].first && y==b.a[i].second)
-        {
-            goto an;
-        }
-      }
-   }
-};
-class border{
-  
-  public:
-  void Draw()
-  { 
-    int i=0;int j=0;
-    for( i=0,j=0;j<20;j++)
-    {  gotoxy(i,j);
-        cout<<"@";
+        return false;
     }
 
-    for(i=74,j=0;j<20;j++)
-    {   gotoxy(i,j);
-       cout<<"@";
-    }
-    for(i=0,j=0;i<75;i++)
-    {   gotoxy(i,j);
-        cout<<"@";
-    }
-    for(i=0,j=20;i<75;i++)
-    {    gotoxy(i,j);
-        cout<<"@";
-    }
-
-  }
-  int check_border(Snake b)
-  {
-        if(b.a[0].first==74 || b.a[0].first==0 || b.a[0].second==0 || b.a[0].second==19)
-       {
-         return 1;
-       }
-       return 0;
-  }
+    friend class Food;
+    friend class Border;
 };
 
-int Snake::h=-22;
-int main(){
-    
-    Snake a;
-    int direction=0;
-    apple b(a);
+class Food {
+private:
+    int x, y;
+
+public:
+    Food(Snake snake_tmp) {
+    an:
+        x = rand() % 72 + 1;
+        y = rand() % 17 + 1;
+        for (int i = 0; i < snake_tmp.length; i++) {
+            if (x == snake_tmp.snake[i].first && y == snake_tmp.snake[i].second) {
+                goto an;
+            }
+        }
+    }
+
+    void Draw() {
+        gotoxy(x, y);
+        cout << "0";
+    }
+
+    bool IsOnFood(Snake& snake_tmp) {
+        if (snake_tmp.snake[0].first == x && snake_tmp.snake[0].second == y) {
+            snake_tmp.score += 1;
+            return true;
+        }
+        return false;
+    }
+
+    void DeletingOldFood(Snake snake_tmp) {
+        gotoxy(x, y);
+        cout << " ";
+        an:
+        x = rand() % 72 + 1;
+        y = rand() % 17 + 1;
+        for (int i = 0; i < snake_tmp.length; i++) {
+            if (x == snake_tmp.snake[i].first && y == snake_tmp.snake[i].second) {
+                goto an;
+            }
+        }
+    }
+};
+
+class Border {
+public:
+    void Draw() {
+        for (int i = 0; i < 75; ++i) {
+            gotoxy(i, 0);
+            cout << "@";
+            gotoxy(i, 20);
+            cout << "@";
+        }
+        for (int j = 0; j < 21; ++j) {
+            gotoxy(0, j);
+            cout << "@";
+            gotoxy(74, j);
+            cout << "@";
+        }
+    }
+
+    bool IsOnBorder(Snake snake_tmp) {
+        if (snake_tmp.snake[0].first >= 74 || snake_tmp.snake[0].first <= 0 || 
+            snake_tmp.snake[0].second <= 0 || snake_tmp.snake[0].second >= 20) {
+            return 1;
+        }
+        return 0;
+    }
+};
+
+void showScore(const Snake& snake_tmp) {
+    gotoxy(76, 1);
+    cout << "Score: " << snake_tmp.score;
+}
+int Snake::h = -22;
+
+int main() {
+    Snake s;
+    int direction = 0;
+    Food food(s);
     char t;
-    border c;
-    pair<int,int> x(100,100);
-    c.Draw();
-    
-    while (1){
-        
-        if (kbhit()){
+    Border border;
+    pair<int, int> x(100, 100);
+    border.Draw();
+
+    while (1) {
+        if (kbhit()) {
             t = getch();
-            if (t=='a') direction = 2;
-            if (t=='w') direction = 3;
-            if (t=='d') direction = 0;
-            if (t=='s') direction= 1;
+            if (t == 'a') direction = 2;
+            if (t == 'w') direction = 3;
+            if (t == 'd') direction = 0;
+            if (t == 's') direction = 1;
         }
-      if(c.check_border(a))
-      {
-         
-          
-          gotoxy(50,5);
-          cout<<" Game over :)) ";
-          break;
-      }
-      
-      a.Draw(x);
-      b.Draw();
-      c.Draw();
-      a.Move(direction,x);
-      
-      if(b.eat_apple( a))
-      { a.up_length();
-        b.delete_apple(a);
-      }
-      
+        if (border.IsOnBorder(s)) {
+            system("cls");
+            gotoxy(50, 5);
+            cout << " GAME OVER !!! ";
+            break;
+        }
+
+        if (s.IsOnBody())
+        {
+            system("cls");
+            gotoxy(50, 5);
+            cout << " GAME OVER !!! ";
+            break;
+        }
+
+        s.Draw(x);
+        food.Draw();
+        showScore(s);
+        s.Move(direction, x);
+
+        if (food.IsOnFood(s)) {
+            s.IncreasingLength();
+            food.DeletingOldFood(s);
+        }
+
         Sleep(150);
     }
 }
-void gotoxy( int column, int line )
-  {
-  COORD coord;
-  coord.X = column;
-  coord.Y = line;
-  SetConsoleCursorPosition(
-    GetStdHandle( STD_OUTPUT_HANDLE ),
-    coord
+
+void gotoxy(int column, int line) {
+    COORD coord;
+    coord.X = column;
+    coord.Y = line;
+    SetConsoleCursorPosition(
+        GetStdHandle(STD_OUTPUT_HANDLE),
+        coord
     );
-  }
+}
